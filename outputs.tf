b84e0f9fe9014884d03b0a3d57ec262754e20ac0
leonardhav <leonard.havekost@web.de>
2024-11-08 21:32:41 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -6,12 +6,12 @@ output "vpc_id" {
 
 output "public_subnets" {
   description = "List of IDs of public subnets"
-  value       = module.vpc.public_subnets[0]
+  value       = module.vpc.public_subnets
 }
 
 output "private_subnets" {
   description = "List of IDs of private subnets"
-  value       = module.vpc.private_subnets[0]
+  value       = module.vpc.private_subnets
 }
 
 # ----- S3 Outputs -----
